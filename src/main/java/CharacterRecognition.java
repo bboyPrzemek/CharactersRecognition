d7f75b2fd8edb7bfbc59a05c01d3ba098ca0f012
Przemek <przemorog01@gmail.com>
2020-02-25 21:58:16 +0100

    added drawing on canvas

--- src/main/java/CharacterRecognition.java
+++ src/main/java/CharacterRecognition.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -18,8 +17,8 @@ public class CharacterRecognition extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
         Scene scene = new Scene(root);
+        scene.getStylesheets().add("Style");
         primaryStage.setResizable(false);
-        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(scene);
         primaryStage.show();
 
--- src/main/java/WindowController.java
+++ src/main/java/WindowController.java
@@ -1,32 +1,48 @@
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
+
 public class WindowController implements Initializable {
 
-    @FXML
-    private FontAwesomeIcon closeBtn;
-    @FXML
-    private FontAwesomeIcon minimizeBtn;
+@FXML private Canvas canvas;
+@FXML private Button clearBtn;
 
     public void initialize(URL location, ResourceBundle resources) {
+       final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
-        closeBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED,
+                new EventHandler<MouseEvent>(){
+                        public void handle(MouseEvent event) {
+                            graphicsContext.beginPath();
+                            graphicsContext.moveTo(event.getX(), event.getY());
+                            graphicsContext.stroke();
+                        }
+
+                });
+
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED,
+                new EventHandler<MouseEvent>(){
+                    public void handle(MouseEvent event) {
+                        graphicsContext.setLineWidth(10);
+                        graphicsContext.lineTo(event.getX(), event.getY());
+                        graphicsContext.stroke();
+                    }
+                });
+
+        clearBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
-               System.exit(0);
+                graphicsContext.clearRect(0,0, canvas.getWidth(),canvas.getHeight());
             }
         });
-        minimizeBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent event) {
-                ((Stage)((FontAwesomeIcon)event.getSource()).getScene().getWindow()).setIconified(true);
-            }
-        });
+
     }
 
 }
